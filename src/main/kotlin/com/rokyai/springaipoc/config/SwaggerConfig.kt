a7ab7529f2713f2c18de1b7b59dde440a0c7b507
rookedsysc <rookedsysc36@gmail.com>
2026-01-18 16:28:37 +0900

    feat(swagger): Swagger UI에 Bearer Token 인증 설정 추가

--- src/main/kotlin/com/rokyai/springaipoc/config/SwaggerConfig.kt
+++ src/main/kotlin/com/rokyai/springaipoc/config/SwaggerConfig.kt
@@ -1,7 +1,10 @@
 package com.rokyai.springaipoc.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.slf4j.LoggerFactory
 import org.springdoc.core.utils.SpringDocUtils
 import org.springframework.context.annotation.Bean
@@ -21,12 +24,24 @@ class SwaggerConfig() {
 
     @Bean
     fun openApi(): OpenAPI {
+        val securitySchemeName = "bearerAuth"
         return OpenAPI()
             .info(
                 Info()
                     .title("Spring AI POC")
                     .description("Spring AI + WebFlux + R2DBC 공부")
             )
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        securitySchemeName,
+                        SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
     }
 }
-
